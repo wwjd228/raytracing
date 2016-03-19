81942a66ba5d91079e657345c5b386c2aec12173
Jungle <wwjd228@gmail.com>
2016-03-20 03:23:00 +0800

    Use force inline
    Because there is a error when using macro.
    
    In this version, use force inline, then the result is correct.

--- math-toolkit.h
+++ math-toolkit.h
@@ -1,24 +1,44 @@
 #ifndef __RAY_MATH_TOOLKIT_H
 #define __RAY_MATH_TOOLKIT_H
-#define subtract_vector(a, b, out) ( out[0] = a[0] - b[0], \
-                                     out[1] = a[1] - b[1], \
-			             out[2] = a[2] - b[2] )
-#define add_vector(a, b, out) ( out[0] = a[0] + b[0], \
-                                out[1] = a[1] + b[1], \
-				out[2] = a[2] + b[2] )
-#define multiply_vectors(a, b, out) ( out[0] = a[0] * b[0], \
-                                      out[1] = a[1] * b[1], \
-				      out[2] = a[2] * b[2] )
-#define multiply_vector(a, b, out) ( out[0] = a[0] * b, \
-                                     out[1] = a[1] * b, \
-				     out[2] = a[2] * b )
 
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
 #include <immintrin.h>
 
-static inline
+static inline __forceinline
+void subtract_vector(const double *a, const double *b, double *out) 
+{
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
+}
+
+static inline __forceinline
+void multiply_vector(const double *a, const double b, double *out) 
+{
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    out[2] = a[2] * b;
+}
+
+static inline __forceinline
+void multiply_vectors(const double *a, const double *b, double *out) 
+{
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    out[2] = a[2] * b[2];
+}
+
+static inline __forceinline
+void add_vector(const double *a, const double *b, double *out)
+{
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    out[2] = a[2] + b[2]; 
+}
+
+static inline __forceinline
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -29,13 +49,13 @@ void normalize(double *v)
     v[2] /= d;
 }
 
-static inline
+static inline __forceinline
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-static inline
+static inline __forceinline
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -43,7 +63,7 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-static inline
+static inline __forceinline
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
@@ -54,7 +74,7 @@ double dot_product(const double *v1, const double *v2)
     return dp;
 }
 
-static inline
+static inline __forceinline
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -62,7 +82,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-static inline
+static inline __forceinline
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
